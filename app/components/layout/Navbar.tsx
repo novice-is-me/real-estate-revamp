0a46d7b8adf2fadf0f01ab6201985577b88220be
novice-is-me <reginajalandoni13@gmail.com>
2026-01-13 21:15:22 +0800

    Navbar

--- app/components/layout/Navbar.tsx
+++ app/components/layout/Navbar.tsx
@@ -1,9 +1,67 @@
-import React from "react";
+"use client";
+import { Menu } from "lucide-react";
+import Image from "next/image";
+import React, { useState } from "react";
 
 const Navbar = () => {
+  const [tabs, setTabs] = useState<object[]>([
+    {
+      name: "Home",
+      link: "#",
+    },
+    { name: "Search", link: "/search" },
+    {
+      name: "Featured",
+      link: "#featured",
+    },
+    {
+      name: "Gallery",
+      link: "#gallery",
+    },
+    { name: "Services", link: "#services" },
+    { name: "About", link: "#about" },
+    { name: "Contact", link: "#contact" },
+  ]);
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
     <nav>
-      <div className=" mx-auto max-w-7xl px-4 flex h-16 items-center justify-between"></div>
+      <div className=" mx-auto max-w-7xl px-4 flex h-25 items-center justify-between ">
+        <Image
+          src="/logo.png"
+          alt="Logo"
+          width={200}
+          height={200}
+          loading="eager"
+        />
+
+        {/* For large view */}
+        <div className=" hidden md:block">
+          {tabs.map((tab, index) => (
+            <div key={index} className="inline-block mx-4">
+              <a href={tab.link}>{tab.name}</a>
+            </div>
+          ))}
+        </div>
+
+        {/* For small view */}
+        <div className="md:hidden">
+          <Menu size={30} onClick={() => setIsMenuOpen((prev) => !prev)} />
+
+          <div>
+            {isMenuOpen && (
+              <div className=" absolute right-4 top-16 w-40 bg-white border flex flex-col items-start px-4 py-2 md:hidden">
+                {tabs.map((tab, index) => (
+                  <div key={index} className=" my-2">
+                    <a href={tab.link}>{tab.name}</a>
+                  </div>
+                ))}
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
     </nav>
   );
 };
